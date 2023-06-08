c5e7fd25d819b9ab9a4edb29c43b63789cc06ead
Aleksei Ogoltcov <aaogoltcov@mai.ru>
2023-06-08 10:24:26 +0300

    Обновлен класс Calculator методами plus, minus, divide, multiply, pow, abs, isPositive, println

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,14 @@
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.function.UnaryOperator;
-
 public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
 
-        BinaryOperator<Integer> plus = (x, y) -> x + y;
-        BinaryOperator<Integer> minus = (x, y) -> x - y;
-        BinaryOperator<Integer> multiply = (x, y) -> x * y;
-        BinaryOperator<Integer> divide = (x, y) -> {
-            if (y == 0) {
-                throw new NullPointerException("Деление на нуль!");
-            }
-
-            return x / y;
-        };
-
-        UnaryOperator<Integer> pow = x -> x * x;
-        UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
-
-        Predicate<Integer> isPositive = x -> x > 0;
-
-        Consumer<Integer> println = System.out::println;
-
         // такая реализация не работает, т.к. calc не предоставляет
-        // методы plus, minus, divide
-        // int a = calc.plus.apply(1, 2);
-        // int b = calc.minus.apply(1,1);
-        // int c = calc.divide.apply(a, b);
+        // методы plus, minus, divide - добавил их в Calculator
+         int a = calc.plus.apply(1, 2);
+         int b = calc.minus.apply(1,1);
+         int c = calc.divide.apply(a, b);
 
-        // рабочий вариант
-        int a = plus.apply(1, 2);
-        int b = minus.apply(1, 1);
-        int c = divide.apply(a, b);
-
-
-        // calc не предоставляет метод println
-        // calc.println.accept(c);
-
-        // рабочий вариант
-        println.accept(c);
+        // calc не предоставляет метод println - добавил в Calculator
+         calc.println.accept(c);
     }
 }
